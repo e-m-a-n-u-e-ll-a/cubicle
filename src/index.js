b1ab2c614a2da5331a864908a1d7e953c6c79fad
e212121 <97968443+e212121@users.noreply.github.com>
2023-03-15 21:11:37 +0200

    Add resources and static files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 let express = require("express");
 let app = express();
+app.use("/static", express.static("public"));
+
+
 app.get("/", (req, res) => {
     res.send("Hi world")
 });
 app.listen(5000, () => {
-console.log("App is listening on port 5000")
+    console.log("App is listening on port 5000")
 })
