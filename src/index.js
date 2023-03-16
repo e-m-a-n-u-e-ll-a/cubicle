00f62ab6a4bca4aa996f9110228273ac34ec3a36
e212121 <97968443+e212121@users.noreply.github.com>
2023-03-16 20:18:34 +0200

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 let express = require("express");
-let app = express();
-app.use("/static", express.static("public"));
+let handlebars = require("express-handlebars");
 
+let app = express();
+
+app.use(express.static('public'));
+
+app.engine("hbs", handlebars.engine({
+    extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", "./src/views")
 
 app.get("/", (req, res) => {
-    res.send("Hi world")
+    res.render("index");
 });
 app.listen(5000, () => {
     console.log("App is listening on port 5000")
