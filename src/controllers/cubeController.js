adbf65362ed944b650c5bda1d2333cf622a69d85
e212121 <97968443+e212121@users.noreply.github.com>
2023-03-16 21:21:11 +0200

    Add about page

---
+++ src/controllers/cubeController.js
@@ -0,0 +1,7 @@
+let router = require("express").Router();
+
+router.get("/create", (req, res) => {
+ res.render("create")
+});
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 let express = require("express");
 
 let homeController = require("./controllers/homeController");
+let cubeController = require("./controllers/cubeController");
 
 let router = express.Router();
 
 router.get("/", homeController.index);
+router.use("/cube", cubeController);
+router.get("/about", homeController.about);
 module.exports = router;
